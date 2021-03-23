38f20663142dbcb5c9abd1517dfe46e2c457c894
Geovane Fedrecheski <geonnave@gmail.com>
2021-03-23 09:22:34 -0300

    feat: add support for CBOR-DI

--- mock_chain/api.py
+++ mock_chain/api.py
@@ -1,41 +1,50 @@
 from flask import Flask, request, make_response, jsonify
 from swarm_lib import DidDocument
-from swarm_sec import SecureBoxCOSE, Keys
+from swarm_sec import Keys, SecureBoxCOSE, SecureBoxJOSE
 from jwcrypto import jwk, jwe
 from cose.keys.cosekey import KeyOps
 import base64, cbor2, cose, json
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 chain = {}
 
 
 @app.route('/dids', methods=["POST"])
 def create_did():
-    """
-    echo '{
-        "signature": ""
-        "did": "did:sw:WbXxdsyQsCEVnHHS7VzTcP",
-        "document": {
-            "id": "did:sw:WbXxdsyQsCEVnHHS7VzTcP",
-            "authentication": ["#vMXfGkJb"]
-        }
-    }' | http :7878/dids
-    """
-    print(">>> post %s" % request.data)
-    if request.content_type == "application/cbor":
-        ddo_cbor = cbor2.loads(request.data).value[2]
-        ddo = DidDocument.from_dict(cbor2.loads(ddo_cbor))
+    print(">>> post %s %s %s" % (len(request.data), request.content_type, request.data))
+    if request.content_type == "application/json":
+        ddo_b64url = json.loads(request.data)["payload"]
+        ddo_str = SecureBoxJOSE.from_b64url(ddo_b64url).decode("utf-8")
+        ddo = DidDocument.from_dict(json.loads(ddo_str))
+        auth_method = ddo.get_verification_method("authentication")
+        print(auth_method.as_jwk())
+        valid, _ = SecureBoxJOSE.verify(request.data, auth_method.as_jwk())
+        if valid:
+            chain[ddo.id.encode()] = ddo
+            return "", 200
+        else:
+            return "", 400
+    else:
+        ddo_binary = cbor2.loads(request.data).value[2]
+        print("DDo binary: %s" % ddo_binary)
+        if request.content_type == "application/cbor":
+            ddo = DidDocument.from_cbor(ddo_binary)
+        elif request.content_type == "application/cbor-di":
+            ddo = DidDocument.from_cbor_di(ddo_binary)
+            print("DDo from cbor-di: %s" % ddo.to_dict())
+
         auth_method = ddo.get_verification_method("authentication")
 
         valid, _ = SecureBoxCOSE.sign1_verify(request.data, auth_method.as_cwk())
         if valid:
             chain[ddo.id.encode()] = ddo
             resp = make_response(b"", 200)
-            resp.headers["content-type"] = "application/cbor"
+            resp.headers["content-type"] = request.content_type
             return resp
         else:
             resp = make_response(b"", 400)
-            resp.headers["content-type"] = "application/cbor"
+            resp.headers["content-type"] = request.content_type
             return resp
 
 
@@ -44,22 +53,27 @@ def resolve_did(did=None):
     """
     http :7878/dids/did:sw:WbXxdsyQsCEVnHHS7VzTcP
     """
-    print(">>> get %s" % did)
+    print(">>> get %s %s" % (did, request.accept_mimetypes))
     if did in chain.keys():
-        if "application/cbor" in request.accept_mimetypes:
-            ddo = chain[did]
+        ddo = chain[did]
+        if "application/cbor-di" in request.accept_mimetypes:
+            resp = make_response(ddo.to_cbor_di(), 200)
+            resp.headers["content-type"] = "application/cbor-di"
+            return resp
+        elif "application/cbor" in request.accept_mimetypes:
             resp = make_response(ddo.to_cbor(), 200)
             resp.headers["content-type"] = "application/cbor"
             return resp
         else:
-            return jsonify({"status": "ok", "document": chain[did].to_dict()}), 200
+            return jsonify({"status": "ok", "didDocument": ddo.to_dict()}), 200
     else:
-        if request.accept_mimetypes == "application/cbor":
+        if "application/cbor" in request.accept_mimetypes:
             resp = make_response(b"", 404)
             resp.headers["content-type"] = "application/cbor"
             return resp
         else:
             return jsonify({"status": "%s not found" % did}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=7878)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,7 @@
 from mock_chain import api
 from swarm_lib import DidDocument
 from jwcrypto import jwk, jwe
-from swarm_sec import SecureBoxCOSE, Keys
+from swarm_sec import Keys, SecureBoxCOSE, SecureBoxJOSE
 import pytest, cbor2
 
 
@@ -16,11 +16,12 @@ def test_get(client):
     resp = client.get("/dids/123")
     assert resp.status_code == 404
 
-def test_post(client):
-    ddo, keys = DidDocument.generate(with_server=True)
-    jk = jwk.JWK(**keys[1])
-    sig_key = Keys.jwk_to_cwk(jk)
-    payload = SecureBoxCOSE.sign1_sign(ddo.to_cbor(), sig_key)
+ddo, keys = DidDocument.generate(with_server=True)
+sig_jwkey = jwk.JWK(**keys[0])
+sig_cwkey = Keys.ensure_cwk(sig_jwkey)
+
+def test_post_cbor(client):
+    payload = SecureBoxCOSE.sign1_sign(ddo.to_cbor(), sig_cwkey)
     print(payload)
 
     resp = client.post("/dids", data=payload, headers={"content-type": "application/cbor"})
@@ -31,4 +32,18 @@ def test_post(client):
     assert resp.status_code == 200
     assert resp.content_type == "application/cbor"
     rddo = DidDocument.from_dict(cbor2.loads(resp.data))
+    assert rddo.id.encode() == ddo.id.encode()
+
+def test_post_json(client):
+    payload = SecureBoxJOSE.sign(ddo.to_json(), sig_jwkey)
+    print(payload)
+
+    resp = client.post("/dids", data=payload, headers={"content-type": "application/json"})
+    assert resp.status_code == 200
+    assert resp.data == b''
+
+    resp = client.get("/dids/%s" % ddo.id.encode(), headers={"accept": "application/json"})
+    assert resp.status_code == 200
+    assert resp.content_type == "application/json"
+    rddo = DidDocument.from_dict(resp.get_json()["didDocument"])
     assert rddo.id.encode() == ddo.id.encode()
